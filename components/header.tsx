b23aa5c399cea9f764eadc0191b9c4f3dc674160
Matthew Stanciu <mattbstanciu@gmail.com>
2022-08-27 17:33:08 -0400

    Begin
    Co-Authored-By: Yash Velagapudi <46730550+yashv6655@users.noreply.github.com>
    Co-Authored-By: Chi-Wei Lien <89671525+chi-wei-lien@users.noreply.github.com>

---
+++ components/header.tsx
@@ -0,0 +1,17 @@
+const Header = () => (
+  <div className="h-screen flex flex-col justify-center items-center">
+    <div className="font-bold">
+      <h1 className="text-center text-4xl sm:text-8xl md:text-9xl text-amber-500">
+        Purdue Hackers
+      </h1>
+      <div className="w-10/12 sm:w-1/2 mx-auto">
+        <p className="text-lg sm:text-xl">
+          We are a community of students who collaborate, learn, and build
+          kick-ass technical projects 💛⚡️
+        </p>
+      </div>
+    </div>
+  </div>
+)
+
+export default Header
