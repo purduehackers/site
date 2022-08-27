00005c1cfae21e0e89359ae9a0b2555fd72b5847
Matthew Stanciu <mattbstanciu@gmail.com>
2022-08-27 17:00:24 -0400

    Add Space Grotesk

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      main: '"Space Grotesk", system-ui, Roboto, sans-serif',
+      sans: '"Inter", sans-serif',
+      mono: '"IBM Plex Mono"',
+    },
+    extend: {
+      colors: {
+        blue: {
+          discord: "#807cfc",
+          "discord-light": "#9b98fa",
+        },
+        amber: {
+          450: "#F8AF18",
+          550: "#E78B09",
+        },
+      },
+      screens: {
+        xs: "375px",
+      },
+    },
   },
   plugins: [],
-}
+};
