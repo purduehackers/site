f47a0801a30b89f9ffadb7e37ee9adbfa4551acb
Matthew Stanciu <mattbstanciu@gmail.com>
2022-08-27 21:20:41 -0400

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  const vercelEnv = process.env.NEXT_PUBLIC_VERCEL_ENV
+  return (
+    <>
+      <Head>
+        <meta name="theme-color" content="#D97706" />
+        <link
+          rel="icon"
+          href={
+            vercelEnv === 'production'
+              ? '/favicon.ico'
+              : vercelEnv === 'preview'
+              ? '/favicon_preview.ico'
+              : '/favicon_dev.ico'
+          }
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
