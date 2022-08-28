4cd20055d2b3b4ba8ee161dc158fbe1043388845
Matthew Stanciu <mattbstanciu@gmail.com>
2022-08-27 23:23:15 -0400

    Some responsive things

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 const Header = () => (
   <div className="h-screen bg-amber-100 flex flex-col justify-center items-center gap-y-4">
-    <div className="shadow-blocks shadow-gray-600 border-4 bg-amber-400 border-black p-6 rounded-md">
-      <h1 className="text-center font-bold text-4xl sm:text-8xl md:text-9xl">
+    <div className="shadow-blocks shadow-gray-600 border-4 bg-amber-400 border-black p-6 rounded-md w-10/12 lg:w-auto">
+      <h1 className="text-center font-bold text-4xl sm:text-8xl lg:text-9xl">
         Purdue Hackers
       </h1>
     </div>
