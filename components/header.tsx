37251a5df3e6087d6ff07221d07d257de4ce3227
chi-wei-lien <lien2@purdue.edu>
2022-08-30 00:00:39 -0400

    make header draggable

--- components/header.tsx
+++ components/header.tsx
@@ -1,18 +1,24 @@
+import Draggable from 'react-draggable';
+
 const Header = () => (
   <div className="py-12 lg:py-0 lg:h-screen bg-amber-100 flex flex-col justify-center items-center gap-y-4">
-    <div className="shadow-blocks shadow-gray-800 border-4 bg-amber-400 border-black p-6 rounded-sm w-10/12 lg:w-auto">
-      <h1 className="text-center font-bold text-6xl sm:text-8xl lg:text-9xl">
-        Purdue Hackers
-      </h1>
-    </div>
-    <div className="w-10/12 sm:w-1/2 mx-auto">
-      <div className="shadow-blocks shadow-gray-800 border-4 bg-white border-black p-4 rounded-sm">
-        <p className="text-lg sm:text-xl font-bold">
-          💛⚡️ a community of students who collaborate, learn, and build
-          kick-ass technical projects
-        </p>
+    <Draggable>
+      <div className="shadow-blocks shadow-gray-800 border-4 bg-amber-400 border-black p-6 rounded-sm w-10/12 lg:w-auto">
+        <h1 className="text-center font-bold text-6xl sm:text-8xl lg:text-9xl">
+          Purdue Hackers
+        </h1>
       </div>
-    </div>
+    </Draggable>
+    <Draggable>
+      <div className="w-10/12 sm:w-1/2 mx-auto">
+        <div className="shadow-blocks shadow-gray-800 border-4 bg-white border-black p-4 rounded-sm">
+          <p className="text-lg sm:text-xl font-bold">
+            💛⚡️ a community of students who collaborate, learn, and build
+            kick-ass technical projects
+          </p>
+        </div>
+      </div>
+    </Draggable>
   </div>
 )
 
