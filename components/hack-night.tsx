c6311cd90156c10bf8761fdd52b6df49ba734790
Lilian Zhao <milanzq309@gmail.com>
2022-10-07 23:25:33 -0400

    added some more stuff

--- components/hack-night.tsx
+++ components/hack-night.tsx
@@ -1,23 +1,47 @@
 import Draggable from "react-draggable"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faWindows } from '@fortawesome/free-brands-svg-icons'
+
 const HackNight = () => (
   <div className="bg-gray-dark min-h-screen">
     <div className="p-6 sm:p-12">
       <h1 className="text-5xl sm:text-9xl font-bold text-white">3. Hack Night</h1>
       <Draggable>
-        <div className="border-solid border-black border-4 cursor-pointer
-            w-1/2 h-52 min-w-fit rounded-xl font-bold font-mono bg-white">
-          <div className="w-full h-6 bg-gray-300 rounded-t-lg 
-              border-black border-solid border-b-4">
-            <div></div>
+        <div className="border-solid border-black border-2 cursor-pointer
+            w-full sm:w-1/2 rounded-xl bg-white relative top-0 right-0">
+          <div className="w-full bg-black rounded-t-lg px-2 text-white font-mono font-bold
+              border-white border-solid border-b-2 flex justify-between items-center">
+            <div><FontAwesomeIcon icon={faWindows} size="1x" /> Window</div>
+            <FontAwesomeIcon icon={faXmark} size="1x" />
           </div>
           <div className="p-6">
-            <h3>The Night is Nigh</h3>
-            <div className="border-solid border-black border-4">
-              <p className="text-sm">
-                Potato poatatotoo
+            <div className="border-solid border-black border-2 p-8">
+              <h2 className="font-bold text-3xl mb-4">The Night is Nigh 🌙</h2>
+              <p className="text-mxs">
+              Hack night is uninterrupted time for you to work on personal projects of 
+              all kinds—anything from a little hack that you ship by the end of the night, 
+              to part of a larger project. what you work on is up to you, but organizers will
+              be there to provide technical & brainstorming help. there's only one rule: no 
+              working on homework. it's friday night, plus you have the rest of the week to work 
+              on your homework! this is time to work on something that excites you.
               </p>
             </div>
+          </div>
+        </div>
+      </Draggable>
+      <Draggable>
+        <div className="border-solid border-white border-2 cursor-pointer
+            w-2/5 h-64 rounded-xl font-bold font-mono bg-black">
+          <div className="w-full bg-gray-300 rounded-t-lg px-2
+              border-black border-solid border-b-4 flex justify-between items-center">
+            <div><FontAwesomeIcon icon={faWindows} size="1x" /> Window</div>
+            <FontAwesomeIcon icon={faXmark} size="1x" />
+          </div>
+          <div className="p-6 text-white">
+            <h3>Nack Hight</h3>
+            <p>Password: </p>
           </div>
         </div>
       </Draggable>
@@ -31,6 +55,7 @@ const HackNight = () => (
           <p className="text-green-500">
             Potato poatatotoo
           </p>
+          <div className="hack-btn text-white border-white">Sign Up</div>
         </div>
       </Draggable>
       <Draggable>
