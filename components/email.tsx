1deaddc7a74986362655f2a6f02f2e008f7814ba
Lilian Zhao <milanzq309@gmail.com>
2022-10-13 02:39:31 -0400

    added other email windows

--- components/email.tsx
+++ components/email.tsx
@@ -12,70 +12,97 @@ const Email = () => {
   return (
     <div className="bg-teal-500 min-h-screen p-24">
       <div className="border-2 border-black flex justify-between items-center bg-white
-            w-11/12 sm:w-4/5 mx-auto h-12 mb-8 shadow-email">
-        <div>
-
+            w-11/12 sm:w-5/6 md:w-4/5 mx-auto h-12 px-8 mb-8 shadow-email">
+        <div className="w-12 flex justify-between text-[8px] text-amber-400">
+          <FontAwesomeIcon icon={faCircle}/>
+          <FontAwesomeIcon icon={faCircle}/>
+          <FontAwesomeIcon icon={faCircle}/>
+        </div>
+        <div className="w-24 flex justify-between text-2xl">
+          <FontAwesomeIcon icon={faEnvelope}/>
+          <FontAwesomeIcon icon={faPaperPlane}/>
+          <FontAwesomeIcon icon={faPencil}/>
         </div>
       </div>
-      <Draggable
-          disabled={!draggable}
-          handle=".handle">
-        <div className="mt-8 border-2 border-black w-11/12 sm:w-[36rem] mx-auto mb-8 shadow-email">
-          <div className="handle border-b-2 border-black flex flex-row bg-gray-300 cursor-pointer">
-            <p className="px-2 border-r-2 border-black bg-red-400">x</p>
-            <div className="grow" />
-            <p>email</p>
-            <div className="grow" />
-            <div />
-          </div>
-          <div className="bg-white pl-2 pr-3 py-2 overflow-scroll h-96 scrollbar scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200">
-            <p className="font-bold">
-              from:{' '}
-              <span className="font-normal">
-                zap@
-                <span className="mx-1 bg-black text-white">
-                  REDACTED
-                </span>
-              </span>
-            </p>
-            <p className="font-bold">
-              subject: <span className="font-normal">an invitation</span>
-            </p>
-            <br />
-            <div className="flex flex-col gap-y-4 text-mxs">
-              <p>Dear Hacker,</p>
-              <p>
-                Shipping a technical project that you’re proud of is among the most
-                validating and rewarding things you can do as a young person.
-                College is the best time in our lives to do it—but actually doing it
-                is soooooooooo hard. Before you can ship something you feel proud
-                of, you have to find something you’d enjoy building, feels unique,
-                and allows you to learn new things, but not too many new things,
-                otherwise you’ll give up. Then, you have to find the time and
-                motivation to actually build the thing—all while being pulled in
-                every direction by academic and social oligations.
-              </p>
-              <p>
-                It’s no surprise most students simply don’t bother, & graduate never
-                having made something they’re proud of.
-              </p>
-              <p>
-                If only there were a community full of friendly, weird, creative, &
-                amazing people people who encouraged you to build projects, & helped
-                you carve out real time in your schedule to do it. A community where
-                you could finally find <span className="italic">your people</span>,
-                who you connect with on a deep level. Who make you feel loved,
-                valued, and seen for who you are, and who invite you to make magic
-                with them.
-              </p>
-              <p>I mean, that would be life-changing, wouldn’t it?</p>
+      <div className="flex w-full">
+        <Draggable
+            disabled={!draggable}
+            handle=".handle">
+          <div className="border-2 border-black w-11/12 sm:w-[36rem] mx-auto mb-8 shadow-email">
+            <div className="handle border-b-2 border-black flex flex-row bg-gray-300 cursor-pointer">
+              <p className="px-2 border-r-2 border-black bg-red-400">x</p>
+              <div className="grow" />
+              <p>email</p>
+              <div className="grow" />
+              <div />
             </div>
-            <br />
-            <p className="text-mxs">Regards,</p>
-            <p className="text-mxs">💛⚡️ The Purdue Hackers community</p>
+            <div className="bg-white pl-2 pr-3 py-2 overflow-scroll h-96 scrollbar scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200">
+              <p className="font-bold">
+                from:{' '}
+                <span className="font-normal">
+                  zap@
+                  <span className="mx-1 bg-black text-white">
+                    REDACTED
+                  </span>
+                </span>
+              </p>
+              <p className="font-bold">
+                subject: <span className="font-normal">an invitation</span>
+              </p>
+              <br />
+              <div className="flex flex-col gap-y-4 text-mxs">
+                <p>Dear Hacker,</p>
+                <p>
+                  Shipping a technical project that you’re proud of is among the most
+                  validating and rewarding things you can do as a young person.
+                  College is the best time in our lives to do it—but actually doing it
+                  is soooooooooo hard. Before you can ship something you feel proud
+                  of, you have to find something you’d enjoy building, feels unique,
+                  and allows you to learn new things, but not too many new things,
+                  otherwise you’ll give up. Then, you have to find the time and
+                  motivation to actually build the thing—all while being pulled in
+                  every direction by academic and social oligations.
+                </p>
+                <p>
+                  It’s no surprise most students simply don’t bother, & graduate never
+                  having made something they’re proud of.
+                </p>
+                <p>
+                  If only there were a community full of friendly, weird, creative, &
+                  amazing people people who encouraged you to build projects, & helped
+                  you carve out real time in your schedule to do it. A community where
+                  you could finally find <span className="italic">your people</span>,
+                  who you connect with on a deep level. Who make you feel loved,
+                  valued, and seen for who you are, and who invite you to make magic
+                  with them.
+                </p>
+                <p>I mean, that would be life-changing, wouldn’t it?</p>
+              </div>
+              <br />
+              <p className="text-mxs">Regards,</p>
+              <p className="text-mxs">💛⚡️ The Purdue Hackers community</p>
+            </div>
+          </div>
+        </Draggable>
+        <div className="border-2 border-black flex flex-col bg-white
+              w-11/12 sm:w-1/6 md:w-64 h-96 mb-8 shadow-email">
+          <div className="inbox-email">
+                <h4>an invitation</h4>
+          </div>
+          <div className="border-2 border-black flex flex-col bg-white
+              w-11/12 sm:w-1/6 md:w-64 h-12 p-8">
+                <h4>***IMPORTANT!***</h4>
+          </div>
+          <div className="border-2 border-black flex flex-col bg-white
+              w-11/12 sm:w-1/6 md:w-64 h-12 p-8">
+                <h4>Salmonella...</h4>
+          </div>
+          <div className="border-2 border-black flex flex-col bg-white
+              w-11/12 sm:w-1/6 md:w-64 h-12 p-8">
+                <h4>waaawaawaaawwwawawa</h4>
           </div>
         </div>
-      </Draggable>
+      </div>
     </div>
   );
 }
