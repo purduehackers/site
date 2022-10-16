bc1c38c9e7aec791e05d8432e2f225be51b756a8
Matthew Stanciu <mattbstanciu@gmail.com>
2022-10-15 21:42:39 -0400

    Run Prettier

--- components/email.tsx
+++ components/email.tsx
@@ -1,117 +1,185 @@
-import Draggable from "react-draggable";
-import { useState, useEffect, useContext } from "react";
+import Draggable from 'react-draggable'
+import { useState, useEffect, useContext } from 'react'
 
-import { DraggableContext, DraggableInterface } from "../context/DraggableContext";
+import {
+  DraggableContext,
+  DraggableInterface
+} from '../context/DraggableContext'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faPaperPlane, faPencil, faCircle, faEnvelopeOpen } from '@fortawesome/free-solid-svg-icons'
+import {
+  faEnvelope,
+  faPaperPlane,
+  faPencil,
+  faCircle,
+  faEnvelopeOpen
+} from '@fortawesome/free-solid-svg-icons'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 
-import { emails } from '../utils/data';
+import { emails } from '../utils/data'
 
 const Email = () => {
-  const { draggable, setDraggable } = useContext(DraggableContext);
+  const { draggable, setDraggable } = useContext(DraggableContext)
 
-  const [open, setOpen] = useState([true, false, false, false, false, false]);
-  const [read, setRead] = useState([true, false, false, false, false, false]);
+  const [open, setOpen] = useState([true, false, false, false, false, false])
+  const [read, setRead] = useState([true, false, false, false, false, false])
 
-  const [password, setPassword] = useState("");
-  const [showPW, setShowPW] = useState(false);
+  const [password, setPassword] = useState('')
+  const [showPW, setShowPW] = useState(false)
 
   return (
     <div className="bg-teal-500 min-h-screen p-24">
       <div className="flex flex-col w-full lg:w-4/5 mx-auto ">
-        <div className="border-2 border-black flex justify-between items-center bg-white
-              w-full h-12 px-8 mb-8 shadow-email shadow-gray-900/70">
+        <div
+          className="border-2 border-black flex justify-between items-center bg-white
+              w-full h-12 px-8 mb-8 shadow-email shadow-gray-900/70"
+        >
           <div className="w-12 flex justify-between text-[8px] text-amber-400">
-            <FontAwesomeIcon icon={faCircle}/>
-            <FontAwesomeIcon icon={faCircle}/>
-            <FontAwesomeIcon icon={faCircle}/>
+            <FontAwesomeIcon icon={faCircle} />
+            <FontAwesomeIcon icon={faCircle} />
+            <FontAwesomeIcon icon={faCircle} />
           </div>
           <div className="w-24 flex justify-between text-2xl">
-            <FontAwesomeIcon icon={faEnvelope} 
+            <FontAwesomeIcon
+              icon={faEnvelope}
               className="text-gray-300"
-              onClick={() => setShowPW(true)}/>
-            <FontAwesomeIcon icon={faPaperPlane} 
+              onClick={() => setShowPW(true)}
+            />
+            <FontAwesomeIcon
+              icon={faPaperPlane}
               className="cursor-pointer hover:text-amber-300"
-              onClick={() => setShowPW(true)}/>
-            <FontAwesomeIcon icon={faPencil} 
+              onClick={() => setShowPW(true)}
+            />
+            <FontAwesomeIcon
+              icon={faPencil}
               className="cursor-pointer hover:text-amber-300"
-              onClick={() => setShowPW(true)}/>
+              onClick={() => setShowPW(true)}
+            />
           </div>
         </div>
-        {showPW &&
+        {showPW && (
           <Draggable
-              disabled={!draggable}
-              handle=".handle"
-              defaultPosition={{x: 250, y: 60 }}>
-            <div className="border-2 border-black w-5/12 sm:w-96 sm:min-w-fit mx-auto
-                shadow-email shadow-gray-900/30 h-fit absolute z-[100]">
+            disabled={!draggable}
+            handle=".handle"
+            defaultPosition={{ x: 250, y: 60 }}
+          >
+            <div
+              className="border-2 border-black w-5/12 sm:w-96 sm:min-w-fit mx-auto
+                shadow-email shadow-gray-900/30 h-fit absolute z-[100]"
+            >
               <div className="handle border-b-2 border-black flex flex-row bg-gray-800 cursor-pointer">
-                <p className="px-2 border-r-2 border-black bg-red-400 hover:bg-red-500"
-                  onClick={() => setShowPW(false)}>x</p>
+                <p
+                  className="px-2 border-r-2 border-black bg-red-400 hover:bg-red-500"
+                  onClick={() => setShowPW(false)}
+                >
+                  x
+                </p>
                 <div className="grow" />
                 <p className="text-white">password</p>
                 <div className="grow" />
               </div>
               <div className="bg-black text-white p-8 flex flex-col justify-center items-center">
-                <input className="bg-black w-3/5 py-1 text-center"
+                <input
+                  className="bg-black w-3/5 py-1 text-center"
                   type="password"
                   value={password}
-                  onChange={(e) => setPassword(e.target.value)} />
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
             </div>
           </Draggable>
-        }
+        )}
         <div className="flex flex-col flex-col-reverse md:flex-row md:flex-row-reverse justify-between w-full">
-          <div className="overflow-scroll scrollbar scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200 min-w-fit
-                border-4 border-black flex flex-col bg-white w-11/12 sm:w-7/12 md:w-64 h-[32rem] shadow-email shadow-gray-900/70">
+          <div
+            className="overflow-scroll scrollbar scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200 min-w-fit
+                border-4 border-black flex flex-col bg-white w-11/12 sm:w-7/12 md:w-64 h-[32rem] shadow-email shadow-gray-900/70"
+          >
             {emails.map((email, i) => {
               return (
-                <div 
-                    className={`border-b-2 border-black flex flex-col bg-white w-full p-4 overflow-y-hidden
-                      hover:bg-gray-100 cursor-pointer ${!read[i] && "border-r-0 border-r-amber-300"}`}
-                    key={i}
-                    onClick={() => {
-                      let newOpen = open;
-                      newOpen[i] = true;
-                      setOpen(newOpen => [...newOpen]);
+                <div
+                  className={`border-b-2 border-black flex flex-col bg-white w-full p-4 overflow-y-hidden
+                      hover:bg-gray-100 cursor-pointer ${
+                        !read[i] && 'border-r-0 border-r-amber-300'
+                      }`}
+                  key={i}
+                  onClick={() => {
+                    let newOpen = open
+                    newOpen[i] = true
+                    setOpen((newOpen) => [...newOpen])
 
-                      let newRead = read;
-                      newRead[i] = true;
-                      setRead(newRead => [...newRead]);
-                    }}>
+                    let newRead = read
+                    newRead[i] = true
+                    setRead((newRead) => [...newRead])
+                  }}
+                >
                   <div className="flex justify-between">
                     <div className="flex items-center">
-                      {!read[i] && <div className="bg-blue-400 w-2 h-2 rounded ml-1 mr-4"></div>}
-                      {open[i] && <FontAwesomeIcon icon={faEnvelopeOpen} className="text-gray-300 mr-4"/>}
-                      {(!open[i] && read[i]) && <FontAwesomeIcon icon={faEnvelope} className="text-amber-300 mr-4"/>}
+                      {!read[i] && (
+                        <div className="bg-blue-400 w-2 h-2 rounded ml-1 mr-4"></div>
+                      )}
+                      {open[i] && (
+                        <FontAwesomeIcon
+                          icon={faEnvelopeOpen}
+                          className="text-gray-300 mr-4"
+                        />
+                      )}
+                      {!open[i] && read[i] && (
+                        <FontAwesomeIcon
+                          icon={faEnvelope}
+                          className="text-amber-300 mr-4"
+                        />
+                      )}
                       <div>
-                        <p className="text-xs text-gray-400 font font-sans">{email.author}</p>
-                        <h4 className={`${!read[i] && "font-medium text-black-400"}`}>{email.subject}</h4>
+                        <p className="text-xs text-gray-400 font font-sans">
+                          {email.author}
+                        </p>
+                        <h4
+                          className={`${
+                            !read[i] && 'font-medium text-black-400'
+                          }`}
+                        >
+                          {email.subject}
+                        </h4>
                       </div>
                     </div>
-                    {false && <FontAwesomeIcon icon={faEnvelope} className="text-gray-300"/>}
-                    {false && <FontAwesomeIcon icon={faEnvelopeOpen} className="text-gray-300"/>}
+                    {false && (
+                      <FontAwesomeIcon
+                        icon={faEnvelope}
+                        className="text-gray-300"
+                      />
+                    )}
+                    {false && (
+                      <FontAwesomeIcon
+                        icon={faEnvelopeOpen}
+                        className="text-gray-300"
+                      />
+                    )}
                   </div>
                 </div>
-              );
+              )
             })}
           </div>
-          {open[0] && 
+          {open[0] && (
             <Draggable
-                disabled={!draggable}
-                handle=".handle"
-                defaultPosition={{x:20, y:20}}>
-              <div className="border-2 border-black w-11/12 sm:w-[36rem] sm:min-w-[30rem] mr-12 
-                  shadow-email shadow-gray-900/30 h-fit z-0">
+              disabled={!draggable}
+              handle=".handle"
+              defaultPosition={{ x: 20, y: 20 }}
+            >
+              <div
+                className="border-2 border-black w-11/12 sm:w-[36rem] sm:min-w-[30rem] mr-12 
+                  shadow-email shadow-gray-900/30 h-fit z-0"
+              >
                 <div className="handle border-b-2 border-black flex flex-row bg-gray-300 cursor-pointer">
-                  <p className="px-2 border-r-2 border-black bg-red-400 hover:bg-red-500"
+                  <p
+                    className="px-2 border-r-2 border-black bg-red-400 hover:bg-red-500"
                     onClick={() => {
-                      let newOpen = open;
-                      newOpen[0] = false;
-                      setOpen(newOpen => [...newOpen]);
-                    }}>x</p>
+                      let newOpen = open
+                      newOpen[0] = false
+                      setOpen((newOpen) => [...newOpen])
+                    }}
+                  >
+                    x
+                  </p>
                   <div className="grow" />
                   <p>email</p>
                   <div className="grow" />
@@ -122,9 +190,7 @@ const Email = () => {
                     from:{' '}
                     <span className="font-normal">
                       zap@
-                      <span className="mx-1 bg-black text-white">
-                        REDACTED
-                      </span>
+                      <span className="mx-1 bg-black text-white">REDACTED</span>
                     </span>
                   </p>
                   <p className="font-bold">
@@ -134,28 +200,31 @@ const Email = () => {
                   <div className="flex flex-col gap-y-4 text-mxs">
                     <p>Dear Hacker,</p>
                     <p>
-                      Shipping a technical project that you’re proud of is among the most
-                      validating and rewarding things you can do as a young person.
-                      College is the best time in our lives to do it—but actually doing it
-                      is soooooooooo hard. Before you can ship something you feel proud
-                      of, you have to find something you’d enjoy building, feels unique,
-                      and allows you to learn new things, but not too many new things,
-                      otherwise you’ll give up. Then, you have to find the time and
-                      motivation to actually build the thing—all while being pulled in
-                      every direction by academic and social oligations.
+                      Shipping a technical project that you’re proud of is among
+                      the most validating and rewarding things you can do as a
+                      young person. College is the best time in our lives to do
+                      it—but actually doing it is soooooooooo hard. Before you
+                      can ship something you feel proud of, you have to find
+                      something you’d enjoy building, feels unique, and allows
+                      you to learn new things, but not too many new things,
+                      otherwise you’ll give up. Then, you have to find the time
+                      and motivation to actually build the thing—all while being
+                      pulled in every direction by academic and social
+                      oligations.
                     </p>
                     <p>
-                      It’s no surprise most students simply don’t bother, & graduate never
-                      having made something they’re proud of.
+                      It’s no surprise most students simply don’t bother, &
+                      graduate never having made something they’re proud of.
                     </p>
                     <p>
-                      If only there were a community full of friendly, weird, creative, &
-                      amazing people people who encouraged you to build projects, & helped
-                      you carve out real time in your schedule to do it. A community where
-                      you could finally find <span className="italic">your people</span>,
-                      who you connect with on a deep level. Who make you feel loved,
-                      valued, and seen for who you are, and who invite you to make magic
-                      with them.
+                      If only there were a community full of friendly, weird,
+                      creative, & amazing people people who encouraged you to
+                      build projects, & helped you carve out real time in your
+                      schedule to do it. A community where you could finally
+                      find <span className="italic">your people</span>, who you
+                      connect with on a deep level. Who make you feel loved,
+                      valued, and seen for who you are, and who invite you to
+                      make magic with them.
                     </p>
                     <p>I mean, that would be life-changing, wouldn’t it?</p>
                   </div>
@@ -165,46 +234,57 @@ const Email = () => {
                 </div>
               </div>
             </Draggable>
-          }
+          )}
           {emails.map((email, i) => {
-            if (i != 0 && open[i]) return (
-              <Draggable
+            if (i != 0 && open[i])
+              return (
+                <Draggable
                   disabled={!draggable}
                   handle=".handle"
-                  defaultPosition={{x: -100 - 40 * i, y: 10 + 18 * i}}>
-                <div className={`border-2 border-black w-11/12 sm:w-[32rem] sm:min-w-[28rem]
-                    shadow-email shadow-gray-900/30 h-fit absolute z-[${i}0]`}>
-                  <div className="handle border-b-2 border-black flex flex-row bg-gray-300 cursor-pointer">
-                    <p className="px-2 border-r-2 border-black bg-red-400 hover:bg-red-500"
-                      onClick={() => {
-                        let newOpen = open;
-                        newOpen[i] = false;
-                        setOpen(newOpen => [...newOpen]);
-                      }}>x</p>
-                    <div className="grow" />
-                    <p>email</p>
-                    <div className="grow" />
-                  </div>
-                  <div className="bg-white pl-2 pr-3 py-2 overflow-scroll h-fit max-h-[26rem] scrollbar scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200">
-                    <p className="font-bold">
-                      from: <span className="font-normal">{email.email}</span>
-                    </p>
-                    <p className="font-bold">
-                      subject: <span className="font-normal">{email.subject}</span>
-                    </p>
-                    <br />
-                    <div className="flex flex-col gap-y-4 text-mxs overflow-x-hidden">
-                      <p dangerouslySetInnerHTML={{__html: email.content}}></p>
+                  defaultPosition={{ x: -100 - 40 * i, y: 10 + 18 * i }}
+                >
+                  <div
+                    className={`border-2 border-black w-11/12 sm:w-[32rem] sm:min-w-[28rem]
+                    shadow-email shadow-gray-900/30 h-fit absolute z-[${i}0]`}
+                  >
+                    <div className="handle border-b-2 border-black flex flex-row bg-gray-300 cursor-pointer">
+                      <p
+                        className="px-2 border-r-2 border-black bg-red-400 hover:bg-red-500"
+                        onClick={() => {
+                          let newOpen = open
+                          newOpen[i] = false
+                          setOpen((newOpen) => [...newOpen])
+                        }}
+                      >
+                        x
+                      </p>
+                      <div className="grow" />
+                      <p>email</p>
+                      <div className="grow" />
+                    </div>
+                    <div className="bg-white pl-2 pr-3 py-2 overflow-scroll h-fit max-h-[26rem] scrollbar scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-200">
+                      <p className="font-bold">
+                        from: <span className="font-normal">{email.email}</span>
+                      </p>
+                      <p className="font-bold">
+                        subject:{' '}
+                        <span className="font-normal">{email.subject}</span>
+                      </p>
+                      <br />
+                      <div className="flex flex-col gap-y-4 text-mxs overflow-x-hidden">
+                        <p
+                          dangerouslySetInnerHTML={{ __html: email.content }}
+                        ></p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Draggable>
-            );
+                </Draggable>
+              )
           })}
         </div>
       </div>
     </div>
-  );
+  )
 }
 
 export default Email
