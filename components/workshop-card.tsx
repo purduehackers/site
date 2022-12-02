317f025fb3848aade7818fcfe0e6349a46fe08d9
chi-wei-lien <lien2@purdue.edu>
2022-12-02 10:57:22 -0500

    change workshop to mobile-first

--- components/workshop-card.tsx
+++ components/workshop-card.tsx
@@ -12,27 +12,27 @@ const WorkshopCard = (props: IEvent) => {
   const eventUrl = "https://events.purduehackers.com/" + slugger.slug(props.name)
 
   return(
-    <div className="max-w-sm overflow-hidden bg-white rounded-lg shadow-lg">
-    <div className="px-6 py-4">
-      <a className="mb-2 text-xl font-bold underline text-declaration hover:text-amber-500" href={eventUrl} target="_blank">{props.name}</a>
-      <p className='text-slate-500'>{date.toDateString()}</p>
-      <div className="h-40 overflow-auto text-base text-gray-700">
-        <div dangerouslySetInnerHTML={{__html:parsedDescription}}></div>
+    <div className="max-w-sm mx-5 mt-5 overflow-hidden bg-white rounded-lg shadow-lg">
+      <div className="px-6 py-4">
+        <a className="mb-2 text-xl font-bold underline text-declaration hover:text-amber-500" href={eventUrl} target="_blank">{props.name}</a>
+        <p className='text-slate-500'>{date.toDateString()}</p>
+        <div className="h-40 overflow-auto text-base text-gray-700">
+          <div dangerouslySetInnerHTML={{__html:parsedDescription}}></div>
+        </div>
+      </div>
+      <div className='relative w-full h-52'>
+        <Image
+          src={props.img}
+          layout='fill'
+          objectFit='contain'
+        ></Image>
+      </div>
+      
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">{props.location}</span>
+        <span className="inline-block px-3 py-1 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">RSVP: {props.rsvp}</span>
       </div>
     </div>
-    <div className='relative w-full h-52'>
-      <Image
-        src={props.img}
-        layout='fill'
-        objectFit='contain'
-      ></Image>
-    </div>
-    
-    <div className="px-6 pt-4 pb-2">
-      <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">{props.location}</span>
-      <span className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">RSVP: {props.rsvp}</span>
-    </div>
-  </div>
   )
 }
 
--- components/workshops.tsx
+++ components/workshops.tsx
@@ -1,13 +1,26 @@
 import WorkshopCard from "./workshop-card"
 import { useState, useEffect } from "react";
 import IEvent from "../utils/IEvent";
+import Image from "next/image";
 
 const Workshops = ({ fetchedEvents }) => {
   return(
     <div className="bg-[url('/img/news_paper_bg.jpg')] bg-cover font-alegre">
       <div className="min-h-screen">
         <div className="mt-3">
-          <div className="grid grid-cols-5">
+          <div className="grid grid-cols-2">
+            <h1 className="p-5 text-5xl text-right">Workshops</h1>
+            <div className="relative w-full h-full">
+              <Image
+                src={"/ph-logo.png"}
+                alt="Purdue Hackers Logo"
+                layout='fill'
+                objectFit='contain'
+              ></Image>
+            </div>
+          </div>
+          
+          {/* <div className="grid grid-cols-5">
             <div className="p-5">
               <div className="p-2 border-4 border-slate-800">
                 <h1 className="font-bold">Today's Weather:</h1>
@@ -16,7 +29,7 @@ const Workshops = ({ fetchedEvents }) => {
               </div>
             </div>
             <div className="col-span-3">
-              <h1 className="p-5 text-center text-8xl">Workshops</h1>
+              <h1 className="p-5 text-5xl text-center">Workshops</h1>
             </div>
             <div className="p-5">
               <div className="p-2 border-4 border-slate-800">
@@ -24,15 +37,17 @@ const Workshops = ({ fetchedEvents }) => {
                 We will have sponsors very soon :D
               </div>
             </div>
+          </div> */}
+          <div className="w-full h-0.5 mt-3 bg-slate-700"></div>
+          <div className="grid grid-cols-5">
+            
+            {/* mayber link price to send us a coffee? */}
+            <div className="text-sm text-center uppercase">Price 1$</div>
+            <div className="col-span-3 text-center capitalize text-md">Purdue Hackers Inc</div>
+            <div className="text-sm text-center uppercase">Issue # 10</div>
           </div>
-          <div className="w-full h-1 mt-3 bg-slate-700"></div>
-          <div className="grid grid-cols-3">
-            <div className="text-2xl text-center uppercase">Price 10 Cents</div>
-            <div className="text-2xl text-center capitalize">Purdue Hackers Inc</div>
-            <div className="text-2xl text-center uppercase">Issue # 10</div>
-          </div>
-          <div className="w-full h-1 bg-slate-700"></div>
-          <div className="grid grid-cols-3 mt-4 justify-items-center">
+          <div className="w-full h-0.5 bg-slate-700"></div>
+          <div className="grid lg:grid-cols-3 justify-items-center sm:grid-cols-1">
             {fetchedEvents.map((event) => {
               return(
                 <WorkshopCard
