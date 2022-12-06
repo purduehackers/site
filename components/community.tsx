aebca7cfb48e8dbcb6b88d87fc57396e54e09a73
Matthew Stanciu <mattbstanciu@gmail.com>
2022-12-06 17:39:26 -0500

    Just return

--- components/community.tsx
+++ components/community.tsx
@@ -1,58 +1,47 @@
 import DiscordWindow from './discord-window'
 import { discordMessages } from '../utils/data'
-import { useEffect, useState } from 'react'
 
-const Community = () => {
-  const [messagesHeight, setMessagesHeight] = useState(0)
-  useEffect(() => {
-    const height = document.getElementById('messages')?.offsetHeight
-    console.log('height', height)
-    if (height) {
-      setMessagesHeight(height)
-    }
-  }, [])
-  return (
-    <div className="bg-yellow-discord-role">
-      <div className="flex flex-col p-6 sm:p-12">
-        <h1 className="text-5xl sm:text-9xl font-bold">1. Community</h1>
-        <div id="messages" className="flex flex-col items-center mt-8">
-          {discordMessages.map((message, i) => (
-            <DiscordWindow key={i} {...message} />
-          ))}
-          <div className="flex flex-col gap-2 absolute translate-y-40 w-11/12 p-4 border-4 border-black shadow-email shadow-gray-900/70 bg-white max-w-xl">
-            <h1 className="font-bold text-3xl sm:text-4xl text-center mb-3">
-              Find Your People 🌈
-            </h1>
-            <p>
-              The heart & soul of Purdue Hackers is our community on Discord
-              with 1000+ people. Share what you've made in{' '}
-              <span className="channel-name">#ship</span>, chat about rainbows
-              in <span className="channel-name">#lounge</span>, share your music
-              musings in <span className="channel-name">#music</span>, ask for
-              technical help in <span className="channel-name">#code</span>
-              —wherever you look, you'll find friendly, kind, radically
-              inclusive, and <span className="italic">really cool</span> people.
-              It's also where we announce upcoming events—in particular,{' '}
-              <span className="channel-name">#announcements</span>.
-            </p>
-            <p>
-              If you go to Purdue, or are planning to go to Purdue, we would
-              love to see you there.
-            </p>
-            <a
-              href="https://puhack.horse/discord"
-              target="_blank"
-              className="mx-auto mt-4"
-            >
-              <button className="p-2 border-2 border-black hover:bg-blue-discord-light transition duration-100 font-bold text-center mx-auto text-xl">
-                Join the Community
-              </button>
-            </a>
-          </div>
+const Community = () => (
+  <div className="bg-yellow-discord-role">
+    <div className="flex flex-col p-6 sm:p-12">
+      <h1 className="text-5xl sm:text-9xl font-bold">1. Community</h1>
+      <div id="messages" className="flex flex-col items-center mt-8">
+        {discordMessages.map((message, i) => (
+          <DiscordWindow key={i} {...message} />
+        ))}
+        <div className="flex flex-col gap-2 absolute translate-y-40 w-11/12 p-4 border-4 border-black shadow-email shadow-gray-900/70 bg-white max-w-xl">
+          <h1 className="font-bold text-3xl sm:text-4xl text-center mb-3">
+            Find Your People 🌈
+          </h1>
+          <p>
+            The heart & soul of Purdue Hackers is our community on Discord with
+            1000+ people. Share what you've made in{' '}
+            <span className="channel-name">#ship</span>, chat about rainbows in{' '}
+            <span className="channel-name">#lounge</span>, share your music
+            musings in <span className="channel-name">#music</span>, ask for
+            technical help in <span className="channel-name">#code</span>
+            —wherever you look, you'll find friendly, kind, radically inclusive,
+            and <span className="italic">really cool</span> people. It's also
+            where we announce upcoming events—in particular,{' '}
+            <span className="channel-name">#announcements</span>.
+          </p>
+          <p>
+            If you go to Purdue, or are planning to go to Purdue, we would love
+            to see you there.
+          </p>
+          <a
+            href="https://puhack.horse/discord"
+            target="_blank"
+            className="mx-auto mt-4"
+          >
+            <button className="p-2 border-2 border-black hover:bg-blue-discord-light transition duration-100 font-bold text-center mx-auto text-xl">
+              Join the Community
+            </button>
+          </a>
         </div>
       </div>
     </div>
-  )
-}
+  </div>
+)
 
 export default Community
