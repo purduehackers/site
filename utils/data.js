b0284206b53a577d0395a51426b5ceff63a18952
Matthew Stanciu <mattbstanciu@gmail.com>
2022-12-06 14:58:53 -0500

    Add some more data

--- utils/data.js
+++ utils/data.js
@@ -90,6 +90,36 @@ export const discordMessages = [
     pfpImageSrc:
       'https://cdn.discordapp.com/avatars/247090061034782721/b033d6798e326464a51351d76ed25232.webp?size=160',
     content: `🦀 🦀 🦀  i made a web api to store my reviews of movies. I call it review-api`
+  },
+  {
+    channel: '#ship',
+    username: 'Neil',
+    pfpImageSrc:
+      'https://cdn.discordapp.com/avatars/221731072822607872/cf31ac5c2be5ee7e596a2face41bb0ac.webp?size=160',
+    content: `made a silly link extender, which puts a ton of loremipsum text as the new link to a webpage`,
+    imageSrc:
+      'https://cdn.discordapp.com/attachments/904896819165814794/1045088556315578409/image.png'
+  },
+  {
+    channel: '#hack-night',
+    username: 'Ayden B',
+    pfpImageSrc:
+      'https://cdn.discordapp.com/avatars/559045506039414784/3fa265211f9a56d23b99c52eab6468ee.webp?size=160',
+    content: 'If the stars align I might be able to join'
+  },
+  {
+    channel: '#lounge',
+    username: 'matthew!!!',
+    pfpImageSrc:
+      'https://cdn.discordapp.com/avatars/89849998585376768/c6f1f84cf99017b24503ffb7eec05c51.webp?size=160',
+    content: `It’s 7~1~f and I’m feeling happy :3`
+  },
+  {
+    channel: '#ship',
+    username: 'Rachel S.',
+    pfpImageSrc:
+      'https://cdn.discordapp.com/avatars/305566021664899072/a_ff1b1103553c76c719211a809df3ab36.gif?size=160',
+    content: `a small victory but i made my first discord bot with python :,) right now it serves as my own personal quotebook but i want it to be my own journal i can scream / rant at`
   }
 ]
 
