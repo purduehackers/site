43500ad0de0fb8939723e20f45cad8c34f4d5f44
Matthew Stanciu <mattbstanciu@gmail.com>
2022-12-06 22:58:50 -0500

    Remove home icon
    We're already home silly!!!

--- components/footer-links.tsx
+++ components/footer-links.tsx
@@ -1,15 +1,7 @@
-import { Home } from 'react-feather'
 import { Instagram, Discord, Github } from '@icons-pack/react-simple-icons'
 
 const FooterLinks = () => (
   <div className="flex flex-row justify-center gap-x-8">
-    <a
-      href="https://purduehackers.com"
-      className="hover:text-gray-600 transition"
-      target="_blank"
-    >
-      <Home height={32} width={32} />
-    </a>
     <a
       href="https://github.com/purduehackers"
       className="hover:text-gray-600 transition"
