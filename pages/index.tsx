0c9bf78d1f9288aa18c9084f6750defd94cc83df
Matthew Stanciu <mattbstanciu@gmail.com>
2022-12-06 23:34:52 -0500

    Add custom og image

--- pages/index.tsx
+++ pages/index.tsx
@@ -74,10 +74,7 @@ const Home: NextPage<HomeFetchedEventsProps> = ({
           <meta property="og:site_name" content="Purdue Hackers" />
           <meta property="og:name" content="Purdue Hackers" />
           <meta property="og:title" content="Purdue Hackers" />
-          <meta
-            property="og:image"
-            content="https://og.purduehackers.com/Home.png?theme=light&md=1&fontSize=250px&caption=%25F0%259F%2592%259B%25E2%259A%25A1%25EF%25B8%258F"
-          />
+          <meta property="og:image" content="img/intro.png" />
           <meta property="og:description" content="💛⚡️" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta property="og:type" content="website" />
