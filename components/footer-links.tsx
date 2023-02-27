b7db46ef68add55f3f9f42810f1b9383533fd32d
Matthew Stanciu <mattbstanciu@gmail.com>
2023-02-27 14:51:25 -0500

    Add Twitter and Mastodon to footer

--- components/footer-links.tsx
+++ components/footer-links.tsx
@@ -1,27 +1,48 @@
-import { Instagram, Discord, Github } from '@icons-pack/react-simple-icons'
+import {
+  Instagram,
+  Discord,
+  Github,
+  Twitter,
+  Mastodon
+} from '@icons-pack/react-simple-icons'
 
 const FooterLinks = () => (
   <div className="flex flex-row justify-center gap-x-8">
     <a
       href="https://github.com/purduehackers"
-      className="hover:text-gray-600 transition"
+      className="transition hover:scale-110"
       target="_blank"
     >
       <Github height={32} width={32} />
     </a>
     <a
+      href="https://puhack.horse/discord"
+      className="transition hover:scale-110 hover:text-discord-vibrant"
+      target="_blank"
+    >
+      <Discord height={32} width={32} />
+    </a>
+    <a
       href="https://instagram.com/purduehackers"
-      className="hover:text-gray-600 transition"
+      className="transition hover:scale-110 hover:text-[#c32aa3]"
       target="_blank"
     >
       <Instagram height={32} width={32} />
     </a>
     <a
-      href="https://puhack.horse/discord"
-      className="hover:text-gray-600 transition"
+      href="https://twitter.com/purduehackers"
+      className="transition hover:scale-110 hover:text-[#1DA1F2]"
       target="_blank"
     >
-      <Discord height={32} width={32} />
+      <Twitter height={32} width={32} />
+    </a>
+    <a
+      href="https://mastodon.boiler.social/@purduehackers"
+      rel="me"
+      className="transition hover:scale-110 hover:text-[#563acc]"
+      target="_blank"
+    >
+      <Mastodon height={32} width={32} />
     </a>
   </div>
 )
