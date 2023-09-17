e191383046e2e140404038e1044e0aeeede4e8cd
Matthew Stanciu <mattbstanciu@gmail.com>
2023-09-17 17:43:54 -0400

    little more stuff

--- components/sponsor/components.js
+++ components/sponsor/components.js
@@ -1,4 +1,22 @@
 export const MDXComponents = {
-  h1: (props) => <h1 {...props} className="text-3xl font-bold mt-4" />,
-  h2: (props) => <h2 {...props} className="text-2xl font-bold" />
+  h1: (props) => (
+    <h1
+      {...props}
+      className="font-main text-3xl font-bold mt-4 text-amber-500"
+    />
+  ),
+  h2: (props) => (
+    <h2 {...props} className="font-main text-2xl font-bold text-amber-500" />
+  ),
+  a: (props) => (
+    <a
+      target="_blank"
+      className="text-amber-500 hover:text-amber-400 transition"
+      style={{
+        textDecoration: 'underline',
+        textUnderlineOffset: '4px'
+      }}
+      {...props}
+    ></a>
+  )
 }
--- components/sponsor/content.tsx
+++ components/sponsor/content.tsx
@@ -3,7 +3,7 @@ import { MDXComponents } from './components'
 
 export function Content({ children }: { children: React.ReactNode }) {
   return (
-    <div className="font-serif">
+    <div className="font-serif flex flex-col gap-4">
       <MDXProvider components={MDXComponents}>{children}</MDXProvider>
     </div>
   )
