b62319a800e6884f1f0d7dfe61460081b028b73e
Matthew Stanciu <mattbstanciu@gmail.com>
2023-09-18 11:04:56 -0400

    Fix custom components
    thanks nextjs docs https://nextjs.org/docs/pages/building-your-application/configuring/mdx#custom-elements

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
-    rehypePlugins: []
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
+    providerImportSource: '@mdx-js/react'
   }
 })
 
