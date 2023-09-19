958f57b26e029025b7b4268fd1095d7cbae409e5
Matthew Stanciu <mattbstanciu@gmail.com>
2023-09-19 19:51:43 -0400

    Update Tip + edits

--- components/sponsor/tip.tsx
+++ components/sponsor/tip.tsx
@@ -1,11 +1,11 @@
 import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export function Tip({ children }: { children: React.ReactNode }): JSX.Element {
+export function Tip({ text }: { text: string }): JSX.Element {
   return (
     <div className="bg-amber-200 rounded-lg flex flex-row justify-center align-center gap-2 p-2 w-9/12 mx-auto">
       <FontAwesomeIcon icon={faInfoCircle} className="pt-1" />
-      {children}
+      <p className="text-base">{text}</p>
     </div>
   )
 }
