e9344fe95b217197e60f6ee63200b957bcdabbb1
Matthew Stanciu <mattbstanciu@gmail.com>
2023-09-20 11:30:39 -0400

    update

--- components/sponsor/grid.tsx
+++ components/sponsor/grid.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-export function Grid({ children }: { children: React.ReactNode }): JSX.Element {
+export function Grid({
+  children,
+  largeGap
+}: {
+  largeGap?: boolean
+  children: React.ReactNode
+}): JSX.Element {
   return (
-    <div className="grid grid-cols-2 gap-2 items-center print:mx-auto">
+    <div
+      className={`grid grid-cols-2 ${
+        largeGap ? 'gap-4' : 'gap-2'
+      } items-center print:mx-auto`}
+    >
       {children}
     </div>
   )
