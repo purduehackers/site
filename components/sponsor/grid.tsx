3d61715affc3d10eee6c72065b5107f02363b34b
Matthew Stanciu <mattbstanciu@gmail.com>
2023-09-20 11:13:27 -0400

    update

--- components/sponsor/grid.tsx
+++ components/sponsor/grid.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
 export function Grid({ children }: { children: React.ReactNode }): JSX.Element {
-  return <div className="grid grid-cols-2 gap-2 print:mx-auto">{children}</div>
+  return (
+    <div className="grid grid-cols-2 gap-2 items-center print:mx-auto">
+      {children}
+    </div>
+  )
 }
--- components/sponsor/tip.tsx
+++ components/sponsor/tip.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export function Tip({ text }: { text: string }): JSX.Element {
   return (
-    <div className="bg-amber-200 rounded-lg flex flex-row justify-center align-center gap-2 p-2 w-9/12 mx-auto">
+    <div className="bg-amber-200 rounded-lg flex flex-row justify-center align-center gap-2 p-2 w-9/12 mx-auto break-inside-avoid">
       <FontAwesomeIcon icon={faInfoCircle} className="pt-1" />
       <p className="text-base">{text}</p>
     </div>
