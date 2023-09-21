9c3405d89c15032405e8aa337d8744fa9a1a5267
Matthew Stanciu <mattbstanciu@gmail.com>
2023-09-21 11:31:24 -0400

    add tiers

--- components/sponsor/tier.tsx
+++ components/sponsor/tier.tsx
@@ -11,11 +11,11 @@ export function Tier({
     <div className="font-main bg-amber-300 rounded-sm border-2 border-black flex flex-col gap-2 p-2 break-inside-avoid shadow-blocks-sm">
       <div className="flex flex-col">
         <h1 className="text-2xl font-bold">{title}</h1>
-        <p className="text-gray-600 font-mono">
+        <p className="text-gray-600 font-mono text-lg">
           {price ? `$${price.toLocaleString('en-US')}` : 'Contact us'}
         </p>
       </div>
-      <p>{description}</p>
+      <p className="text-base">{description}</p>
     </div>
   )
 }
