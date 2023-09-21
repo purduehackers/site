64becb64fe1fd2ea74852a4e2efeb4552b141b5e
Matthew Stanciu <mattbstanciu@gmail.com>
2023-09-21 17:56:41 -0400

    Revert "Fix mobile layout"
    This reverts commit 13028d1fcce7da1811ca2b9d452b31b8c413db35.

--- components/sponsor/grid.tsx
+++ components/sponsor/grid.tsx
@@ -5,24 +5,20 @@ export function Grid({
   children,
   largeGap,
   overflow,
-  singleRow,
   className
 }: {
   children: React.ReactNode
   largeGap?: boolean
   overflow?: boolean
-  singleRow?: boolean
   className?: string
 }): JSX.Element {
   return (
     <div
       className={clsx(
         className,
-        `grid items-center break-inside-avoid ${
-          singleRow ? 'grid-cols-1 md:grid-cols-2' : 'grid-cols-2'
-        } ${largeGap ? 'gap-4' : 'gap-2'} ${
-          overflow ? 'md:sponsor-overflow' : ''
-        }`
+        `grid grid-cols-2 items-center break-inside-avoid ${
+          largeGap ? 'gap-4' : 'gap-2'
+        } ${overflow ? 'sponsor-overflow' : ''}`
       )}
     >
       {children}
