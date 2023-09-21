66d3d4b8b8c1d6501dbe004650522ed5671945da
Matthew Stanciu <mattbstanciu@gmail.com>
2023-09-21 12:03:53 -0400

    most popular

--- components/sponsor/tier.tsx
+++ components/sponsor/tier.tsx
@@ -1,14 +1,27 @@
 export function Tier({
   title,
   description,
-  price
+  price,
+  mostPopular
 }: {
   title: string
   description: string
   price?: number
+  mostPopular?: boolean
 }): JSX.Element {
   return (
-    <div className="font-main bg-amber-300 rounded-sm border-2 border-black flex flex-col gap-2 p-2 break-inside-avoid shadow-blocks-sm">
+    <div
+      className={`font-main ${
+        mostPopular
+          ? 'bg-gradient-to-br from-amber-100 to-amber-400 relative'
+          : 'bg-amber-300'
+      } rounded-sm border-2 border-black flex flex-col gap-2 p-2 break-inside-avoid shadow-blocks-sm`}
+    >
+      {mostPopular ? (
+        <div className="absolute top-0 right-0 transform rotate-12 print:rotate-[35deg] print:translate-x-4 print:translate-y-2 bg-white border-2 border-black px-1 rounded-sm">
+          <p className="text-sm">Most Popular</p>
+        </div>
+      ) : null}
       <div className="flex flex-col">
         <h1 className="text-2xl font-bold">{title}</h1>
         <p className="text-gray-600 font-mono text-lg">
