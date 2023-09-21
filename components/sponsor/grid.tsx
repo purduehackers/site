21c79ac808b17cb0a1af1fb359ffddf217be4f73
Matthew Stanciu <mattbstanciu@gmail.com>
2023-09-21 12:09:49 -0400

    Add clsx

--- components/sponsor/grid.tsx
+++ components/sponsor/grid.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
+import clsx from 'clsx'
 
 export function Grid({
   children,
   largeGap,
-  overflow
+  overflow,
+  className
 }: {
   children: React.ReactNode
   largeGap?: boolean
   overflow?: boolean
+  className?: string
 }): JSX.Element {
   return (
     <div
-      className={`grid grid-cols-2 ${
-        largeGap ? 'gap-4' : 'gap-2'
-      } items-center ${overflow ? 'sponsor-overflow' : ''}`}
+      className={clsx(
+        className,
+        `grid grid-cols-2 ${largeGap ? 'gap-4' : 'gap-2'} items-center ${
+          overflow ? 'sponsor-overflow' : ''
+        }`
+      )}
     >
       {children}
     </div>
