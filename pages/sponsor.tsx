b1bc226823134c2569cdc2c25af2ab63467d5373
Matthew Stanciu <mattbstanciu@gmail.com>
2023-09-25 20:09:27 -0400

    add meta tags to sponsor page

--- pages/sponsor.tsx
+++ pages/sponsor.tsx
@@ -1,9 +1,21 @@
+import Head from 'next/head';
 import { Content } from '../components/sponsor/content';
 import SponsorContent from '../components/sponsor/sponsor.mdx';
 
 export default function Sponsor(): JSX.Element {
   return (
     <div className="min-h-screen flex flex-col">
+      <Head>
+        <meta property="og:site_name" content="Purdue Hackers" />
+        <meta property="og:name" content="Purdue Hackers" />
+        <meta property="og:title" content="Sponsor | Purdue Hackers" />
+        <meta
+          property="og:description"
+          content="All the most talented, creative, & dedicated engineers congregate in Purdue Hackers to make real projects. Get directly in front of them by sponsoring us."
+        />
+        <meta property="og:type" content="website" />
+        <title>Sponsor | Purdue Hackers</title>
+      </Head>
       <header className="border-b-4 bg-amber-300 print:bg-amber-300 border-black">
         <div className="py-16 flex flex-col gap-y-4">
           <h1 className="text-center text-4xl sm:text-7xl print:text-7xl sm:tracking-tight font-bold flex justify-center items-center gap-2">
